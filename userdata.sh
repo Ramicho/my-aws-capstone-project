fdd063d312def0351a3f2f1b5ab2893caedfd060
Ramicho <ramazanozkoklu@gmail.com>
2022-10-17 23:08:30 +0300

    files updated

--- userdata.sh
+++ userdata.sh
@@ -3,13 +3,13 @@ apt-get update -y
 apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
-TOKEN=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
-git clone https://$TOKEN@github.com/Ramicho/private-myprojects.git
-cd /home/ubuntu/private-myprojects/capstoneProject
+TOKEN="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"
+git clone https://$TOKEN@github.com/Ramicho/my-aws-capstone-project.git
+cd /home/ubuntu/my-aws-capstone-project
 apt install python3-pip -y
 apt-get install python3.7-dev libmysqlclient-dev -y
 pip3 install -r requirements.txt
-cd /home/ubuntu/private-myprojects/capstoneProject/src
+cd /home/ubuntu/my-aws-capstone-project/src
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
